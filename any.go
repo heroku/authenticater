dcdf56fc0e7ba4ee331a8b1e7901a51477ceece6
Edward Muller <edwardam@interlix.com>
2015-03-10 16:05:37 -0700

    Update docs

--- any.go
+++ any.go
@@ -2,6 +2,7 @@ package authenticater
 
 import "net/http"
 
+// AnyOrNoAuth just returns true for any call to Authenticate
 type AnyOrNoAuth struct{}
 
 func (fa AnyOrNoAuth) Authenticate(r *http.Request) bool {
