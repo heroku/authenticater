52263e586eae3d8673444f49a161f15b372fae30
Edward Muller <edwardam@interlix.com>
2015-03-10 17:17:52 -0700

    Ensure we're not racing
    Before this go test -v -race ./.. would fail with the new test

--- basic_auth.go
+++ basic_auth.go
@@ -4,25 +4,27 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"sync"
 )
 
-// BasicAuth will handle normal user/password Basic Auth
-// It handles multiple valid passwords for the same user.
+// BasicAuth handles normal user/password Basic Auth requests, multiple
+// password for the same user and is safe for concurrent use.
 type BasicAuth struct {
+	sync.RWMutex
 	creds map[string][]string
 }
 
 // NewBasicAuth returns an empty BasicAuth Authenticator
-func NewBasicAuth() BasicAuth {
-	return BasicAuth{
+func NewBasicAuth() *BasicAuth {
+	return &BasicAuth{
 		creds: make(map[string][]string),
 	}
 }
 
 // NewBasicAuthFromString creates and populates a BasicAuth from the provided
-// credentials, encoded as a string of the following format:
+// credentials, encoded as a string, in the following format:
 // user:password|user:password|...
-func NewBasicAuthFromString(creds string) (BasicAuth, error) {
+func NewBasicAuthFromString(creds string) (*BasicAuth, error) {
 	ba := NewBasicAuth()
 	for _, u := range strings.Split(creds, "|") {
 		uparts := strings.SplitN(u, ":", 2)
@@ -37,22 +39,27 @@ func NewBasicAuthFromString(creds string) (BasicAuth, error) {
 
 // AddPrincipal add's a user/password combo to the list of valid combinations
 func (ba *BasicAuth) AddPrincipal(user, pass string) {
+	ba.Lock()
 	u, existed := ba.creds[user]
 	if !existed {
 		u = make([]string, 0, 1)
 	}
 	ba.creds[user] = append(u, pass)
+	ba.Unlock()
 }
 
-// Authenticate returns true if the Request has a valid BasicAuth signature and
-// that signature encodes a username/password that the BasicAuth knows
-func (ba BasicAuth) Authenticate(r *http.Request) bool {
+// Authenticate is true if the Request has a valid BasicAuth signature and
+// that signature encodes a known username/password combo.
+func (ba *BasicAuth) Authenticate(r *http.Request) bool {
 	user, pass, ok := r.BasicAuth()
 	if !ok {
 		return false
 	}
 
-	if passwords, ok := ba.creds[user]; ok {
+	ba.RLock()
+	defer ba.RUnlock()
+
+	if passwords, exists := ba.creds[user]; exists {
 		for _, password := range passwords {
 			if password == pass {
 				return true
