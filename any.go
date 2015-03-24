16ff6099109815e5523480d377232dc6021ba3cf
Edward Muller <edwardam@interlix.com>
2015-03-23 20:18:03 -0700

    Update for lint

--- any.go
+++ any.go
@@ -5,6 +5,7 @@ import "net/http"
 // AnyOrNoAuth just returns true for any call to Authenticate
 type AnyOrNoAuth struct{}
 
+// Authenticate all requests
 func (fa AnyOrNoAuth) Authenticate(r *http.Request) bool {
 	return true
 }
